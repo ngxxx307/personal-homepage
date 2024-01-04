ea3e96b005f8a4ddc4103733014c047ad2e5c9a1
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-01-04 19:32:01 +0800

    add background

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ function App() {
   const [page, setPage] = useState('/')
   
   return (
-    <Router className="background min-h-screen min-w-full">
+    <Router>
+      <div className="background min-h-screen min-w-full relative">
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Blog" element={<Blog />} />
       </Routes>
+      </div>
       {/* TODO: Warning: Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it. */}
       {/* {window.onbeforeunload = function () {window.scrollTo(0, 0);}} */}
     </Router>

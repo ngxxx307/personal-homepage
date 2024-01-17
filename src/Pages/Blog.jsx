ce1966526c4b3ba3b6cfb62c423bcb29551fb0df
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-01-17 22:15:23 +0800

    css mobile test

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -6,7 +6,7 @@ import ArticleList from "../Components/Blog/ArticleList";
 
 const Blog = () => {
   return (
-    <div>
+    <div className="w-full">
       <div className="px-24">
         <motion.p className="text-oliveGreen font-syne text-center text-9xl my-8 font-semibold" {...fadeInDropAnimation}>
             Blog

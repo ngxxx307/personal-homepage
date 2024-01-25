fb728ca5bc08ce7b48a282557e054b3a6a65ee8a
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-01-25 19:42:49 +0800

    Add error boundary in edit/ create popup

--- src/Components/MarkdownDisplayer.jsx
+++ src/Components/MarkdownDisplayer.jsx
@@ -8,33 +8,66 @@ import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import rehypeRaw from 'rehype-raw'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  resetErrorBoundary() {
+    this.setState({ hasError: false });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
 const MarkdownDisplayer = ({ markdown }) => {
-  return (
-    <div className="prose">
-      <ReactMarkdown
-        children={markdown}
-        remarkPlugins={[remarkMath, remarkGfm, emoji]}
-        rehypePlugins={[rehypeKatex, rehypeRaw]}
-        components={{
-          code(props) {
-            const { children, className, node, ...rest } = props;
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter
-                {...rest}
-                PreTag="div"
-                children={String(children).replace(/\n$/, "")}
-                language={match[1]}
-                style={atomOneDark}
-              />
-            ) : (
-              <code {...rest} className={className}>{children}</code>
-            );
-          },
-        }}
-      />
-    </div>
-  );
-};
+    return (
+      <ErrorBoundary>
+      <div className="prose">
+        <ReactMarkdown
+          children={markdown}
+          remarkPlugins={[remarkMath, remarkGfm, emoji]}
+          rehypePlugins={[rehypeKatex, rehypeRaw]}
+          components={{
+            code(props) {
+              const { children, className, node, ...rest } = props;
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  PreTag="div"
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  style={atomOneDark}
+                />
+              ) : (
+                <code {...rest} className={className}>{children}</code>
+              );
+            },
+          }}
+        />
+      </div>
+      </ErrorBoundary>
+    );
+  }
+
 
 export default MarkdownDisplayer;

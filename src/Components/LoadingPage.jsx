ef4da153063f25f83563ae047834840b8db12bbd
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-02-05 21:16:04 +0800

    Add responsive layout to LoadingPage.jsx

--- src/Components/LoadingPage.jsx
+++ src/Components/LoadingPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const DummyArticle = () => {
+  let tagsNumber = 2;
+  console.log(window.innerWidth)
+  if (window.innerWidth > 1024) {
+    tagsNumber = 5
+  }
   return (
     <div className='my-2 font-roboto text-medium px-16 py-4' >
       <div className='flex flex-col mt-2 gap-y-2'>
@@ -11,8 +16,8 @@ const DummyArticle = () => {
         <div className='flex grow gap-x-2'>
         {(()  => {
           const td = []
-          for (let i = 0; i < 5; i++) {
-            td.push(<div className='px-2 py-1 rounded-3xl bg-gray-300 bg-opacity-75 loading-background w-[5%] h-8'></div>)
+          for (let i = 0; i < tagsNumber; i++) {
+            td.push(<div className='px-2 py-1 rounded-3xl bg-gray-300 bg-opacity-75 loading-background w-[33%] lg:w-[5%] h-8'></div>)
           }
             return td
           })()
